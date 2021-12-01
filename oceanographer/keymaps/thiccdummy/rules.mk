bef3e2719b6ecfb2c13565c9971c5686668a3fc5
thiccdummy <95075096+thiccdummy@users.noreply.github.com>
2021-11-30 22:47:03 -0700

    Add files via upload

---
+++ oceanographer/keymaps/thiccdummy/rules.mk
@@ -0,0 +1,17 @@
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
+ifeq ($(strip $(OLED_ENABLE)), yes)
+    #... your code here...
+
+    ifdef OCEAN_DREAM_ENABLE
+        ifeq ($(strip $(OCEAN_DREAM_ENABLE)), yes)
+            SRC += ocean_dream.c
+            OPT_DEFS += -DOCEAN_DREAM_ENABLE
+        endif
+    endif
+    ifndef OCEAN_DREAM_ENABLE
+        SRC += ocean_dream.c
+        OPT_DEFS += -DOCEAN_DREAM_ENABLE
+    endif
+endif
+
+
