d58cd4d1cb6d74f7eb41f10db7450b5631edba9d
thiccdummy <95075096+thiccdummy@users.noreply.github.com>
2021-11-30 23:02:48 -0700

    Update keymap.c

--- oceanographer/keymaps/thiccdummy/keymap.c
+++ oceanographer/keymaps/thiccdummy/keymap.c
@@ -1,3 +1,7 @@
+#ifdef OCEAN_DREAM_ENABLE
+#    include "ocean_dream.h"
+#endif 
+
 #include QMK_KEYBOARD_H
 
 enum layer_names {
@@ -10,8 +14,6 @@ enum layer_names {
 #define _1_SPC LT(_FIRST, KC_SPC)
 #define _2_SPC LT(_SECOND, KC_SPC)
 #define _2_PSCR LT(_FIRST, KC_PSCREEN)
-#define is_calm
-#define render_stars
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_all(
